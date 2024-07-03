8f4dbae875985dc4e12e947a95642cec0e7c235e
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2024-07-03 04:54:09 +0100

    idk

--- src/components/rAndH/Chef.tsx
+++ src/components/rAndH/Chef.tsx
@@ -3,12 +3,13 @@ import Map from '../Map'
 import Image from 'next/image'
 import restauts from '@/assets/restauts.png'
 import MapChef from '../MapChef'
+import hotelthree from '@/assets/hotelthree.jpg'
 
 export default function Chef() {
   return (
     <div className='w-[90%] h-[70%] flex flex-row justify-between items-start space-x-3'>
         <div className=''>
-            <Image src={restauts} alt='restauts' height={350} className='rounded-xl'/>
+            <Image src={hotelthree} alt='restauts' height={350} className='rounded-xl'/>
         </div>
         <div className=''>
             <MapChef/>
--- src/components/rAndH/Oua.tsx
+++ src/components/rAndH/Oua.tsx
@@ -4,12 +4,13 @@ import Image from 'next/image'
 import restauts from '@/assets/restauts.png'
 import MapChef from '../MapChef'
 import MapOua from '../MapOua'
+import hotelTwo from '@/assets/hoteltwo.jpg'
 
 export default function Oua() {
   return (
     <div className='w-[90%] h-[70%] flex flex-row justify-between items-start space-x-3'>
         <div className=''>
-            <Image src={restauts} alt='restauts' height={350} className='rounded-xl'/>
+            <Image src={hotelTwo} alt='restauts' height={350} className='rounded-xl'/>
         </div>
         <div className=''>
             <MapOua/>
