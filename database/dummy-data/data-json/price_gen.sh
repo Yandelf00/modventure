85e286ef5f909bb94a22db3e685db2de595bd78c
Yeeloman <yami.onlyme@gmail.com>
2024-07-03 01:31:10 +0100

    more upgrades

--- database/dummy-data/data-json/price_gen.sh
+++ database/dummy-data/data-json/price_gen.sh
@@ -6,19 +6,18 @@ generate_random_number() {
 }
 
 # Check if the correct number of arguments is passed
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <input_file> <output_file>"
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <input_file> "
   exit 1
 fi
 
 # Input and output file arguments
 input_file="$1"
-output_file="$2"
 
 # Use awk to replace the occurrences with a random number
-awk -v min=100 -v max=5000 'BEGIN { srand(); } { gsub(/"price": [0-9]+/, "\"price\": " min+rand()*max) } 1' "$input_file" >"$output_file"
+awk -v min=100 -v max=5000 'BEGIN { srand(); } { gsub(/"price": [0-9]+/, "\"price\": " min+rand()*max) } 1' "$input_file" >file.json
 
 rm $input_file
-mv $output_file $input_file
+mv file.json $input_file
 
-echo "Processing complete. Check the output file: $output_file"
+echo "Processing complete. Check the output file: $input_file"
